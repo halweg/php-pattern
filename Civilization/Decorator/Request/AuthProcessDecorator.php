e1e66d5cd841b7901ff8e0ba50c05412b708235d
halweg <halweg@126.com>
2020-11-24 20:50:38 +0800

    no message

--- Civilization/Decorator/Request/AuthProcessDecorator.php
+++ Civilization/Decorator/Request/AuthProcessDecorator.php
@@ -8,7 +8,6 @@ class AuthProcessDecorator extends ProcessDecorator
 {
     public function process(RequestHelper $requestHelper)
     {
-        // TODO: Implement process() method.
         echo __CLASS__."Do Auth Things \n";
         $this->process->process($requestHelper);
     }
